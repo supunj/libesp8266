06da4588e792b6132ba5fedf0b83569cfa251e63
Sudiv Akshitha <supunj@gmail.com>
2015-11-25 22:35:32 +0530

    Adding eclipse related files

---
+++ .ino.cpp
@@ -0,0 +1,13 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-11-25 20:09:24
+
+#include "Arduino.h"
+#include "esp8266util.h"
+#include "eepromutil.h"
+void setup() ;
+void loop() ;
+
+#include "esp8266_common.ino"
+
